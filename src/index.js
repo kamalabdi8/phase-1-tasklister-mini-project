38251f1b7b51736ea592c5626b1e0aff5a284619
kamalabdi8 <kamalabdi042@gmail.com>
2024-10-06 00:11:38 +0300

    Submit Challenge

--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
   taskForm.addEventListener("submit", function (event) {
    event.preventDefault();
  
+   const color = document.getElementById("select-colour").value;
    const taskDescription = document.getElementById("new-task-description").value;
    if (taskDescription.trim() !== ""){
-     addTaskToList(taskDescription);
+     addTaskToList(taskDescription, color);
      taskForm.reset();
    }
   });
  });
  
- function addTaskToList(task) {
+ function addTaskToList(task, color) {
    const taskList = document.getElementById("tasks");
    const newTask = document.createElement("li");
    newTask.textContent = task;
+   newTask.style.color = color;
  
    const deleteButton = document.createElement("button")
    deleteButton.textContent = "Delete";
@@ -24,5 +26,5 @@ document.addEventListener("DOMContentLoaded", () => {
    });
  
    newTask.appendChild(deleteButton);
-   taskList.appendChild(newTask);
+   taskList.appendChild(newTask,);
  }
