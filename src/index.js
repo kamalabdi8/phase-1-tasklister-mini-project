de5fcee031918b0d84c98c1bcce76b291fe92c04
kamalabdi8 <kamalabdi042@gmail.com>
2024-10-05 18:36:10 +0300

    code  challenge  2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const taskForm = document.getElementById("create-task-form");
+ 
+  taskForm.addEventListener("submit", function (event) {
+   event.preventDefault();
+ 
+   const taskDescription = document.getElementById("new-task-description").value;
+   if (taskDescription.trim() !== ""){
+     addTaskToList(taskDescription);
+     taskForm.reset();
+   }
+  });
+ });
+ 
+ function addTaskToList(task) {
+   const taskList = document.getElementById("tasks");
+   const newTask = document.createElement("li");
+   newTask.textContent = task;
+ 
+   const deleteButton = document.createElement("button")
+   deleteButton.textContent = "Delete";
+   deleteButton.addEventListener("click", () => {
+     newTask.remove();
+   });
+ 
+   newTask.appendChild(deleteButton);
+   taskList.appendChild(newTask);
+ }
