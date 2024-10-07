8156a82f631f8760239ba98d51989fad6eab0358
kamalabdi8 <kamalabdi042@gmail.com>
2024-10-07 09:30:59 +0300

    Task

--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // getting the form element using its ID
   const taskForm = document.getElementById("create-task-form");
- 
+
+  // putting in the form submission
   taskForm.addEventListener("submit", function (event) {
-   event.preventDefault();
- 
-   const color = document.getElementById("select-colour").value;
-   const taskDescription = document.getElementById("new-task-description").value;
-   if (taskDescription.trim() !== ""){
-     addTaskToList(taskDescription, color);
-     taskForm.reset();
-   }
+    event.preventDefault();
+
+    // putting the values from the form inputs using its ID
+    const color = document.getElementById("select-colour").value;
+    const taskDescription = document.getElementById("new-task-description").value;
+
+    // validating the form input and adding the task
+    if (taskDescription.trim() !== "") {
+      addTaskToList(taskDescription, color);
+      taskForm.reset();
+    }
   });
- });
- 
- function addTaskToList(task, color) {
-   const taskList = document.getElementById("tasks");
-   const newTask = document.createElement("li");
-   newTask.textContent = task;
-   newTask.style.color = color;
- 
-   const deleteButton = document.createElement("button")
-   deleteButton.textContent = "Delete";
-   deleteButton.addEventListener("click", () => {
-     newTask.remove();
-   });
- 
-   newTask.appendChild(deleteButton);
-   taskList.appendChild(newTask,);
- }
+});
+
+// adding a function with two arguments
+function addTaskToList(task, color) {
+  // creating a task element with a list that represents the task
+  const taskList = document.getElementById("tasks");
+  const newTask = document.createElement("li");
+  newTask.textContent = task;
+  newTask.style.color = color;
+
+  // creating a delete button
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => {
+    newTask.remove();
+  }); 
+
+  // appending task and button to the list
+  newTask.appendChild(deleteButton);
+  taskList.appendChild(newTask); 
+}
